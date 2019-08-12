c1cfdd5b37008ea47c8423a612399e91c614937b
hiroo takaku <slgerkamp@gmail.com>
2019-08-12 20:38:49 +0900

    try to use SimpMessagingTemplate

--- src/main/java/com/slgerkamp/introductory/spring/boot/websocket/stomp/application/controller/GreetingController.java
+++ src/main/java/com/slgerkamp/introductory/spring/boot/websocket/stomp/application/controller/GreetingController.java
@@ -2,17 +2,24 @@ package com.slgerkamp.introductory.spring.boot.websocket.stomp.application.contr
 
 import com.slgerkamp.introductory.spring.boot.websocket.stomp.application.model.Greeting;
 import com.slgerkamp.introductory.spring.boot.websocket.stomp.application.model.HelloMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
+    public void greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        simpMessagingTemplate.convertAndSend(
+                "/topic/greetings",
+                "Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,13 +13,13 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/gs-guide-websocket');
+    var socket = new SockJS('/websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/greetings', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
+            showGreeting(greeting.body);
         });
     });
 }
